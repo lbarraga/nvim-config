c489fd9dee9c3dd4000227dc6cc27cd11852468f
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2025-08-13 02:30:24 +0200

    feat[terminal]: make CTRL+L work in terminal mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,9 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("TermEnter", {
+  callback = function(ev)
+    vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = ev.buf, nowait = true })
+  end,
+})
