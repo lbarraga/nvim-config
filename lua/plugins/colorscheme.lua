171b2c7726603b6bfb82990db9eb319bc5657c1b
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2025-12-16 11:29:34 +0100

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,10 +3,18 @@ return {
 
   { "rmehri01/onenord.nvim" },
 
+  { "xiantang/darcula-dark.nvim" },
+
+  { "folke/tokyonight.nvim" },
+
+  { "nickkadutskyi/jb.nvim" },
+
+  { "navarasu/onedark.nvim" },
+
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onenord",
+      colorscheme = "onedark",
     },
   },
 }
